5fbc6d84ee0211556334c99b9f12198035147000
alexandramocanu1 <alexandramocanu74@gmail.com>
2025-08-20 14:53:47 +0300

    first commit

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -7,14 +7,17 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBJ-V5XqbcFXRn7wdO_iTbLxsbXZpp0zOM",
-  authDomain: "infinity-math-53be3.firebaseapp.com",
-  projectId: "infinity-math-53be3",
-  storageBucket: "infinity-math-53be3.firebasestorage.app",
-  messagingSenderId: "298237929876",
-  appId: "1:298237929876:web:9f48511cad9d0b9453c781",
-  measurementId: "G-E2V65M6271"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
+
+export default firebaseConfig;
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
